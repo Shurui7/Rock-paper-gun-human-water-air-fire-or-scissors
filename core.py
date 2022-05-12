31ed149d5759155f461bbb4dda047dcd8db04994
Shurui7 <96158419+Shurui7@users.noreply.github.com>
2022-05-12 12:45:07 -0400

    Core of the game

---
+++ core.py
@@ -0,0 +1,36 @@
+# Made by MorinMax and Shurui7_, MIT liscensed
+
+import random
+
+user_input = input('Rock, paper, gun, human, water, air, fire or scissors: ')
+
+rpc_options = ['rock','paper','scissors','fire','gun','human','water','sponge','air']
+
+def checkstatus():
+  if user_input == terminal_response:
+    print('We tied')
+  elif terminal_response in defeats[user_input]:
+    print('You win')
+  else:
+    print('You lose')
+
+if user_input not in rpc_options: 
+  print(f'\nYour answer is incorrect; it should be in this list: {rpc_options}\nYour answer is: {user_input}, do you see the error? Lets try again')
+else:
+  defeats = {
+    'air' : ['fire', 'rock', 'water', 'gun'],
+    'gun' : ['rock', 'fire', 'scissors', 'human'],
+    'paper' : ['air', 'rock', 'water', 'gun'],
+    'rock' : ['scissors', 'sponge', 'fire', 'human'],
+    'scissors' : ['air', 'paper', 'human', 'sponge'],
+    'fire' : ['sponge', 'paper', 'human', 'scissors'],
+    'water' : ['rock', 'fire', 'scissors', 'gun'],
+    'sponge' : ['water', 'paper', 'gun', 'air'],
+    'human' : ['sponge', 'paper', 'air', 'water'],
+    }
+
+  terminal_response = random.choice(rpc_options)
+  
+  print(f'\nYou choose {user_input}, I choose {terminal_response}')
+
+  checkstatus()
